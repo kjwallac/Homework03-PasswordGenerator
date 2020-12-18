43cfe505321297a038cc538503f715b1fba89c7f
kjwallac <kristinajwallace@gmail.com>
2020-12-17 23:20:05 -0800

    created vars for character strings, and for loop - lowercase 8 character password generates correctly

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
+var lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+var upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var specChars = ' !\"#$%&\'()*+,-./:;<=>?@[\]^_\`{|}~';
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  var password = '';
+  var availChars = lowerChars;
+  var passwordLength = 8;
+
+  for (var i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random() * availChars.length);
+    password += availChars[index];
+  }
+  return password;
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
